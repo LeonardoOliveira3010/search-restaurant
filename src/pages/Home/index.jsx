d32d35a578facd89af12da00d62b238d1a6eaf5c
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-11-08 16:01:37 -0300

    Criando a Home do projeto e resetando o estilo

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Container } from "./style";
+
+export const Home = () =>{
+    return <Container>Hello World</Container>
+}
---
+++ src/pages/Home/style.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #051024;
+    color: #fff;
+`
